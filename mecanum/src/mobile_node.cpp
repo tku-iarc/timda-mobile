436d0336d3caee57791d798b60fc22591ccd4c32
NEET <zxc455233@gmail.com>
2020-11-04 02:16:35 +0800

    Publish odom
    Need to test

--- mecanum/src/mobile_node.cpp
+++ mecanum/src/mobile_node.cpp
@@ -24,12 +24,16 @@ class TimdaMobile
     ros::Publisher odom_pub;
     tf::TransformBroadcaster odom_broadcaster;
     ros::Rate *loop_rate;
+    ros::Time current_time, last_time;
     pthread_t t;
     Mecanum *M;
     TimdaModbus *TM;
     double a = 0.48544;
     double b = 0.253;
     double R = 0.1524;
+    double rx = 0.0;
+    double ry = 0.0;
+    double rz = 0.0;
 
 private:
     double RPM;
@@ -40,10 +44,8 @@ public:
         sub_ = nh_.subscribe("mobile/cmd_vel", 1, &TimdaMobile::Callback, this);
         odom_pub = nh_.advertise<nav_msgs::Odometry>("odom", 50);
         loop_rate = new ros::Rate(10);
-        // if (pthread_create(&t, NULL, (THREADFUNCPTR)&TimdaMobile::Looper, this)) {
-        //     printf("node thread error\n");
-        //     exit(EXIT_FAILURE);
-        // }
+        current_time = ros::Time::now();
+        last_time = ros::Time::now();
 
         M = new Mecanum(this->a, this->b, this->R);
         TM = new TimdaModbus();
@@ -73,6 +75,54 @@ public:
             // std::cout<<"Robot's Speed:"<<V.at(0)<<" "
             //                                               <<V.at(1)<<" "
             //                                               <<V.at(2)<<std::endl;
+            current_time = ros::Time::now();
+            double dt = (current_time - last_time).toSec();
+            double delta_x = (V.at(0)* cos(rz) - V.at(1)* sin(rz)) * dt;
+            double delta_y = (V.at(0)* sin(rz) + V.at(1)* cos(rz)) * dt;
+            double delta_th = V.at(2)* dt;
+
+            rx += delta_x;
+            ry += delta_y;
+            rz += delta_th;
+
+            //since all odometry is 6DOF we'll need a quaternion created from yaw
+            geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(rz);
+
+            //first, we'll publish the transform over tf
+            geometry_msgs::TransformStamped odom_trans;
+            odom_trans.header.stamp = current_time;
+            odom_trans.header.frame_id = "odom";
+            odom_trans.child_frame_id = "base_link";
+
+            odom_trans.transform.translation.x = rx;
+            odom_trans.transform.translation.y = ry;
+            odom_trans.transform.translation.z = 0.0;
+            odom_trans.transform.rotation = odom_quat;
+
+            //send the transform
+            odom_broadcaster.sendTransform(odom_trans);
+
+            //next, we'll publish the odometry message over ROS
+            nav_msgs::Odometry odom;
+            odom.header.stamp = current_time;
+            odom.header.frame_id = "odom";
+
+            //set the position
+            odom.pose.pose.position.x = rx;
+            odom.pose.pose.position.y = ry;
+            odom.pose.pose.position.z = 0.0;
+            odom.pose.pose.orientation = odom_quat;
+
+            //set the velocity
+            odom.child_frame_id = "base_link";
+            odom.twist.twist.linear.x = V.at(0);
+            odom.twist.twist.linear.y = V.at(1);
+            odom.twist.twist.angular.z = V.at(2);
+
+            //publish the message
+            odom_pub.publish(odom);
+
+            last_time = current_time;
             ros::spinOnce();
             loop_rate->sleep();
         }
