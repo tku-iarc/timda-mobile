0f3d657b08c01b7a514e372b1b8d37b5cd22b147
DamnTT <thomas19980311@gmail.com>
2020-12-17 17:48:46 +0800

    successful sending the topic to the robot but the value have to fix

--- webserver/pages/js/KeyboardControl.js
+++ webserver/pages/js/KeyboardControl.js
@@ -3,8 +3,18 @@ window.addEventListener("keyup", keyuped, false);
 
 var keys = [];
 var start;
-var vec3;
-
+var vec3 = new ROSLIB.Message({
+          linear : {
+            x : 0.0,
+            y : 0.0,
+            z : 0.0
+          },
+          angular : {
+            x : 0.0,
+            y : 0.0,
+            z : 0.0
+          }
+        });
 function KeyboardState(state) {
     start = state;
 }
@@ -16,82 +26,68 @@ function keysdown(e) {
         
         //RobotControl
         if (keys[87] && keys[68]) {
-            vec3 = new ROSLIB.Message({
-                x: parseFloat(speed / Math.pow(2, 0.5)),
-                y: parseFloat(speed / Math.pow(2, 0.5)),
-                z: 0
-            });
+         
+            vec3.linear.x = parseFloat(speed / Math.pow(2, 0.5));
+            vec3.linear.y = parseFloat(speed / Math.pow(2, 0.5));
+            vec3.angular.z = 0;
             cmdVel.publish(vec3);
-            console.log("fuck");
             //PublishTopicCmdVel(vec3);
         } else if (keys[87] && keys[65]) {
-            vec3 = new ROSLIB.Message({
-                x: -parseFloat(speed / Math.pow(2, 0.5)),
-                y: parseFloat(speed / Math.pow(2, 0.5)),
-                z: 0
-            });
+        
+            vec3.linear.x = -parseFloat(speed / Math.pow(2, 0.5));
+            vec3.linear.y = parseFloat(speed / Math.pow(2, 0.5));
+            vec3.angular.z = 0;
             cmdVel.publish(vec3);
             //PublishTopicCmdVel(vec3);
         } else if (keys[83] && keys[68]) {
-            vec3 = new ROSLIB.Message({
-                x: parseFloat(speed / Math.pow(2, 0.5)),
-                y: -parseFloat(speed / Math.pow(2, 0.5)),
-                z: 0
-            });
+            
+            vec3.linear.x =  parseFloat(speed / Math.pow(2, 0.5));
+            vec3.linear.y = -parseFloat(speed / Math.pow(2, 0.5));
+            vec3.angular.z = 0;
             cmdVel.publish(vec3);
             //PublishTopicCmdVel(vec3);
         } else if (keys[83] && keys[65]) {
-            vec3 = new ROSLIB.Message({
-                x: -parseFloat(speed / Math.pow(2, 0.5)),
-                y: -parseFloat(speed / Math.pow(2, 0.5)),
-                z: 0
-            });
+            
+            vec3.linear.x = -parseFloat(speed / Math.pow(2, 0.5));
+            vec3.linear.y = -parseFloat(speed / Math.pow(2, 0.5));
+            vec3.angular.z = 0;
             cmdVel.publish(vec3);
             //PublishTopicCmdVel(vec3);
         } else if (keys[87]) {
-            vec3 = new ROSLIB.Message({
-                x: 0,
-                y: parseFloat(speed),
-                z: 0
-            });
+            
+            vec3.linear.x = 0;
+            vec3.linear.y = parseFloat(speed) ;
+            vec3.angular.z = 0;
             cmdVel.publish(vec3);
            // PublishTopicCmdVel(vec3);
         } else if (keys[68]) {
-            vec3 = new ROSLIB.Message({
-                x: parseFloat(speed),
-                y: 0,
-                z: 0
-            });
+            
+            vec3.linear.x =  parseFloat(speed) ;
+            vec3.linear.y = 0 ;
+            vec3.angular.z = 0 ;
             cmdVel.publish(vec3);
             //PublishTopicCmdVel(vec3);
         } else if (keys[83]) {
-            vec3 = new ROSLIB.Message({
-                x: 0,
-                y: -parseFloat(speed),
-                z: 0
-            });
+            vec3.linear.x = 0 ;
+            vec3.linear.y = -parseFloat(speed);
+            vec3.angular.z = 0 ;
             cmdVel.publish(vec3);
             //PublishTopicCmdVel(vec3);
         } else if (keys[65]) {
-            vec3 = new ROSLIB.Message({
-                x: -parseFloat(speed),
-                y: 0,
-                z: 0
-            });
+            vec3.linear.x =-parseFloat(speed);
+            vec3.linear.y = 0;
+            vec3.angular.z = 0;
             cmdVel.publish(vec3);
-            //PublishTopicCmdVel(vec3);
-        } else if (keys[69]) {
-            var speed_;
+            //PublishTopicCmdVel(vec3);            
             if (Math.abs(parseFloat(speed)) > 15){
               speed_ = parseFloat(speed) * 0.5;
             }else{
               speed_ = speed;
             }
-            vec3 = new ROSLIB.Message({
-                x: 0,
-                y: 0,
-                z: -parseFloat(speed_)
-            });
+            vec3.linear.x = 0 ;
+            vec3.linear.y = 0 ;
+            vec3.angular.z = -parseFloat(speed_) ;
+
             cmdVel.publish(vec3);
             //PublishTopicCmdVel(vec3);
         } else if (keys[81]) {
@@ -101,47 +97,52 @@ function keysdown(e) {
             }else{
               speed_ = speed;
             }
-            vec3 = new ROSLIB.Message({
-                x: 0,
-                y: 0,
-                z: parseFloat(speed_)
-            });
+            vec3.linear.x = 0;
+            vec3.linear.y = 0;
+            vec3.angular.z = parseFloat(speed_);
+
             cmdVel.publish(vec3);
             //PublishTopicCmdVel(vec3);
         }
 
     }
 }
-
 function releasebutton(state) {
-    let vec3 = new ROSLIB.Message({
-        x: 0,
-        y: 0,
-        z: 0
-    });
+   let vec3 = new ROSLIB.Message({
+          linear : {
+            x : 0.0,
+            y : 0.0,
+            z : 0.0
+          },
+          angular : {
+            x : 0.0,
+            y : 0.0,
+            z : 0.0
+          }
+        });
     switch(state){
       case 81:
-        vec3.z = 0;
+        vec3.angular.z = 0;
         break;
       case 69:
-        vec3.z = 0;
+        vec3.angular.z = 0;
         break;
       case 87:
-        vec3.y = 0;
+        vec3.linear.y = 0;
         break;
       case 65:
-        vec3.x = 0;
+        vec3.linear.x = 0;
         break;
       case 83:
-        vec3.y = 0;
+        vec3.linear.y = 0;
         break;
       case 68:
-        vec3.x = 0;
+        vec3.linear.x = 0;
         break;
       default:
-        vec3.x = 0;
-        vec3.y = 0;
-        vec3.Z = 0;
+        vec3.linear.x = 0;
+        vec3.linear.y = 0;
+        vec3.angular.z = 0;
     }
     //if(state==81||state==69||state==87||state==65||state==83||state==68){
     //    console.log("stop");
