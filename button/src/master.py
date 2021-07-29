0bc5167bf3eb2c862bb47cc44ba1ae355d3ab5e0
DamnTT <thomas19980311@gmail.com>
2021-07-29 17:57:08 +0800

    fix button func

---
+++ button/src/master.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# To recieve what table has been select
+import rospy
+from nodeMCU_python.srv import wifi_srv 
+
+def fun1(req):
+    print('req is : {}'.format(req.num_req))
+    return ["finish!"]
+
+def main():
+    rospy.init_node('server_wifi', anonymous = True)
+    rospy.Service('wifi_module', wifi_srv, fun1)
+    rospy.spin()
+
+if __name__ == '__main__':
+    main() 
