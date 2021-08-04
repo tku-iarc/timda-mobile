6f0f897e8411d90a26a5a693d985b1b2ab8e73e2
DamnTT <thomas19980311@gmail.com>
2021-08-04 23:52:56 +0800

    service done and button function finished

--- button/src/recieve_esp.py
+++ button/src/recieve_esp.py
@@ -1,51 +1,53 @@
 #!/usr/bin/env python
 import rospy
-import serial 
-from nodeMCU_python.srv import wifi_srv 
+import serial
+from button.srv import wifi_srv
 
-#COM_PORT = '/dev/ttyUSB0'    
+#COM_PORT = '/dev/ttyUSB0'
 
-BAUD_RATES = 115200    
+BAUD_RATES = 115200
 
 list_ser = []
 
-def recieve_data():      
-    try: 
+
+def recieve_data():
+    try:
         while True:
-            while ser.in_waiting:          
+            while ser.in_waiting:
                 list_ser.append(ser.read(1))
-            
+
             if len(list_ser) >= 3:
                 if list_ser[0] == 's' and list_ser[2] == 'e':
                     print(list_ser)
                     pass_esp8266_info_to_server(str(ord(list_ser[1])))
-                    del list_ser [:]
+                    del list_ser[:]
                 else:
-                    del list_ser [:]
-            
+                    del list_ser[:]
 
     except KeyboardInterrupt:
-        ser.close()    
+        ser.close()
         print('good bye!')
+
 
 def pass_esp8266_info_to_server(data):
     rospy.wait_for_service('wifi_module')
     try:
-        #create a server object
+        # create a server object
         val = rospy.ServiceProxy('wifi_module', wifi_srv)
-        #val(arg) -> send a req to server
+        # val(arg) -> send a req to server
         resp = val(data)
     except rospy.ServiceException, e:
-        print ('error')
+        print('error')
+
 
 if __name__ == "__main__":
     rospy.init_node("NodeMCU")
 
-    port_name = rospy.get_param('~port','/dev/ttyUSB1')
+    port_name = rospy.get_param('~port', '/dev/ttyUSB1')
     #baud = int(rospy.get_param('~baud','57600'))
     print("try to connect with {port_name}")
 
-    ser = serial.Serial(port_name, BAUD_RATES) 
-    print("connection succeeded!")  
+    ser = serial.Serial(port_name, BAUD_RATES)
+    print("connection succeeded!")
 
     recieve_data()
